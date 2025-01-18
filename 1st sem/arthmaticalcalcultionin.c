456653d6303d60776d728dd4e543f2d63d18794f
SALMAN LAREB KHAN <salmanaxl7643@gmail.com>
2025-01-18 09:44:48 +0530

    Create arthmaticalcalcultionin.c

---
+++ 1st sem/arthmaticalcalcultionin.c
@@ -0,0 +1,13 @@
+# include <stdio.h> 
+int main() {
+int a = 55;
+int b = 99;
+int c = 78;
+int k = 56;
+int r = 54;
+int s = 87;
+int salman = a+b+c+k+r*s ;
+printf("%d", salman);
+printf("\nthe total number is above");
+return 0;
+}
