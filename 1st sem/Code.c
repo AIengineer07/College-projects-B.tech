e41e27380df9ae7823604d7b9a2d1b23028fcc5e
SALMAN LAREB KHAN <salmanaxl7643@gmail.com>
2025-03-17 13:26:37 +0530

    Create Code.c

---
+++ 1st sem/Code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ int main (){
+int a,b,c;
+
+printf("First No ");
+scanf("%d",&a);
+
+printf("Second No");
+scanf("%d",&b);
+
+printf("Third No");
+scanf("%d",&c);
+
+ if (c > b > a)
+        printf("TRUE");
+    else
+        printf("FALSE");
+    return 0;
+}
